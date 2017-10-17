071d5850472e3141328385edcf24a1eff5b3263d
Rahbee Alvee <ralvee@deputy.com>
2017-10-17 13:31:01 +1100

    Password hashing and salting with bcrypt

--- model/user.js
+++ model/user.js
@@ -1,4 +1,8 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+import Debug from 'debug';
+
+const debug = Debug('express-app');
 
 const UserSchema = mongoose.Schema({
     email: {
@@ -21,6 +25,19 @@ const UserSchema = mongoose.Schema({
         type: String,
         required: true,
     }
+});
+
+//hashing a password before saving it to the database
+UserSchema.pre('save',  function(next) {
+    // this is user object
+    bcrypt.hash(this.password, 10, (err, hash) => {
+        if (err) {
+            return next(err);
+        }
+        this.password = hash;
+        this.passwordConf = hash;
+        next();
+    })
 });
 
 const User = mongoose.model('User', UserSchema);
